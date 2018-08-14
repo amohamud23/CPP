2bbdd41d5a5fe7c5d4c0e7e9542bcf79ebfc9617
Abdi Mohamud <amohamud23@gmail.com>
2018-08-13 23:16:28 -0700

    created a vector with initial values to loop through

--- findnumberVector/main.cpp
+++ findnumberVector/main.cpp
@@ -9,7 +9,19 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int main() {
+    vector<int> myvector = {1,2,3,4,5,6,7,8};
+    
+    myvector.push_back(1);
+    myvector.push_back(2);
+    myvector.push_back(3);
+    
+    for ( vector<int>::iterator i = myvector.begin(); i != myvector.end(); ++i)
+    {
+        cout << *i;
+    }
     
     return 0;
 }
