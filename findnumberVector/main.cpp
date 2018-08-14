bd1702cd98192bf028e38bf4bb30a16b06b44eed
Abdi Mohamud <amohamud23@gmail.com>
2018-08-13 23:04:09 -0700

    first commit

--- findnumberVector/main.cpp
+++ findnumberVector/main.cpp
@@ -7,9 +7,9 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
     return 0;
 }
